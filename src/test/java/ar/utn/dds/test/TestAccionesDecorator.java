3a0a6acf2a3e822d865e817aed3849a6cd28b764
Macarena <lepera.macarena@gmail.com>
2016-05-16 00:59:29 -0300

    SuboTestsTDD

---
+++ src/test/java/ar/utn/dds/test/TestAccionesDecorator.java
@@ -0,0 +1,30 @@
+package ar.utn.dds.test;
+
+import org.junit.Test;
+
+import ar.utn.dds.juegoDeDatos.JuegoDeDatos;
+
+public class TestAccionesDecorator extends JuegoDeDatos{
+
+	
+	@Test
+	public void consultaTardaMasDe20segundosYavisoAlAdmin(){
+		
+	}
+
+	@Test
+	public void consultaTardaMasDe2segundosYNoAvisoAlAdmin(){
+		
+	}
+
+	@Test
+	public void seRealizan5ConsultasEnCiertoDia_SeVerificaQueElReporteSeaCorrecto(){
+		
+	}
+
+	@Test
+	public void unUsuarioeRealiza2Consultas_SeVerificaQueElReporteSeaCorrecto(){
+		
+	}
+	
+}
