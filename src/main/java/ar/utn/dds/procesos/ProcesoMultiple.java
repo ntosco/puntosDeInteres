2523df6e15af4086f6b927492566b0d2d5e22229
Macarena <lepera.macarena@gmail.com>
2016-06-15 14:23:27 -0300

    estado de proceso multiple

--- src/main/java/ar/utn/dds/procesos/ProcesoMultiple.java
+++ src/main/java/ar/utn/dds/procesos/ProcesoMultiple.java
@@ -11,14 +11,31 @@ public class ProcesoMultiple extends Proceso{
 	String nombre;
 	int idProcesoMultiple;
 	
+	
 	@Override
 	public void ejecutarse(EstrategiaPorFallo estrategiaDeFallo) {
-		procesosAEjecutar.forEach(
-				proceso -> proceso.ejecutarse(estrategiaDeFallo));
+		procesosAEjecutar.forEach( 
+			proceso -> {
+				proceso.ejecutarse(estrategiaDeFallo);
+				this.actualizacionDeEstado(proceso);
+		}
+		);
 	}
 
 	public void setProcesos(List<Proceso> procesos) {
 		procesosAEjecutar = procesos;
 	}
 	
+	public void actualizacionDeEstado(Proceso proceso){
+		
+			if(this.falloEnSuEjecucion(proceso)){
+				this.getEstado().setValor(1);
+			}else
+				this.getEstado().setValor(2);		
+
+	}
+	
+	public boolean falloEnSuEjecucion(Proceso proceso){
+		return proceso.getEstado().getValor() == 1;
+	}
 }
