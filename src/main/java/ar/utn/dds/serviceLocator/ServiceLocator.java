d052a836cf491ec82914ba0360dd746832db87a2
SofiAudisio <sofiaudisio@gmail.com>
2016-06-15 03:23:45 -0300

    Se agrega clase borrada en commit

---
+++ src/main/java/ar/utn/dds/serviceLocator/ServiceLocator.java
@@ -0,0 +1,54 @@
+package ar.utn.dds.serviceLocator;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.mockito.Mockito.mock;
+
+import ar.utn.dds.reportes.Reporte;
+import ar.utn.dds.reportes.ReporteProcesosEjecutados;
+import ar.utn.dds.servicios.MailSender;
+
+
+public class ServiceLocator {
+	
+	static ServiceLocator instanceServiceLocator;
+
+		
+	private List<Reporte> listaServicios = new  ArrayList<Reporte>();
+
+	private ReporteProcesosEjecutados historialProcesosEjecutados = new ReporteProcesosEjecutados();
+	private MailSender mailSender = mock(MailSender.class);
+	
+	public static ServiceLocator getInstance() {
+		if (instanceServiceLocator == null) {
+			instanceServiceLocator = new ServiceLocator();
+		}	
+		return instanceServiceLocator;
+	}
+	
+	// ********************************************************
+	// ** Getters and Setters
+	// ********************************************************
+	
+	public List<Reporte> getServicios() {
+		return listaServicios;
+	}
+	
+	public void setServicio(Reporte reporte) {
+		this.listaServicios.add(reporte);
+	}
+
+	public ReporteProcesosEjecutados getHistorialProcesosEjecutados() {
+		return historialProcesosEjecutados;
+	}
+
+	public void setHistorialProcesosEjecutados(ReporteProcesosEjecutados historialProcesosEjecutados) {
+		this.historialProcesosEjecutados = historialProcesosEjecutados;
+	}
+	
+	public MailSender getMailSender() {
+		return mailSender;
+	}
+	
+	
+}
