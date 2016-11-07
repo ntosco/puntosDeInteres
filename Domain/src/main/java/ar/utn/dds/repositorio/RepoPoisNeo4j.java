39be892a2d4e48d2d888a52a74421581515887e5
martinsamo <martin-samo@hotmail.com>
2016-11-07 01:24:42 -0300

    agrego agregarReview a repoPois (ver si es responsabilidad)

--- Domain/src/main/java/ar/utn/dds/repositorio/RepoPoisNeo4j.java
+++ Domain/src/main/java/ar/utn/dds/repositorio/RepoPoisNeo4j.java
@@ -55,6 +55,26 @@ public class RepoPoisNeo4j extends AbstractRepoNeo4J {
 	  //  return this.basicSearch((("Poi.nombre =~ \'(?i).*" + valor) + ".*\'"));
 	  }
 	
+	public void agregarReview(String nombreUsuario, String nombrePoi, String valoracion, String review){
+		//validar que no pueda opinar dos veces
+		GraphDatabaseService db = this.getGraphDb();
+		String query = "MATCH (p:Poi {nombre: '" + nombrePoi + "'}), (user: Usuario {nombreUsuario: '" + nombreUsuario + "'}) CREATE (user)-[:Opino {opinion: '" + review + "', valoracion:'" + valoracion + "'}]->(p)";
+		db.execute(query);
+	}
+	
+	public Node getNodoPoiById(final int id) {
+		Iterator<Node> _basicSearch = this.basicSearch(("ID(poi) = " + id));
+		return IteratorExtensions.<Node>head(_basicSearch);
+	}
+	
+	  private Iterator<Node> basicSearch(final String where) {
+		    GraphDatabaseService _graphDb = this.getGraphDb();
+		    final Result result = _graphDb.execute((("match (poi:Poi) where " + where) + " return poi"));
+		    final Iterator<Node> poi_column = result.<Node>columnAs("poi");
+		    return poi_column;
+		  }
+	
+	
 /*	private Iterator<Node> basicSearch(String valor) {
 	    GraphDatabaseService graphDb = this.getGraphDb();
 	    String query = "MATCH (n:Label) where n.username =~ ".*foo regex.*" RETURN n;"
--- Domain/src/test/java/ar/utn/dds/test/TestRepoPoisNeo4J.java
+++ Domain/src/test/java/ar/utn/dds/test/TestRepoPoisNeo4J.java
@@ -4,8 +4,13 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.neo4j.graphdb.Node;
 
+import ar.utn.dds.POI.POI;
+import ar.utn.dds.repositorio.POIToNodeConverter;
 import ar.utn.dds.repositorio.RepoPoisNeo4j;
+import ar.utn.dds.repositorio.UsuarioToNodeConverter;
+import ar.utn.dds.usuarios.UsuarioConcreto;
 
 public class TestRepoPoisNeo4J {
 	RepoPoisNeo4j repoPois;
@@ -19,5 +24,42 @@ public class TestRepoPoisNeo4J {
 	public void testGetPois() {
 		assertEquals(repoPois.getPois("parada 144").size(),  1);
 	}
+	
+	@Test
+	public void testAgregarReview() {
+		//repoPois.agregarReview("fer", "parada 144", "4", "no tarda mucho en venir");
+		assertEquals(repoPois.getPois("parada 144").size(),  1);
+	}
+	
+	@Test
+	public void testGetNodoPoiById() {
+		Node nodoPoi = repoPois.getNodoPoiById(5);
+		assertEquals(nodoPoi.getId(), 5);
+		assertEquals(nodoPoi.getAllProperties().get("nombre"), "parada 144");
+		assertEquals(nodoPoi.getAllProperties().get("direccionNombre"), "Mozart");
+		assertEquals(nodoPoi.getAllProperties().get("direccionNumero"), "2300");
+		assertEquals(nodoPoi.getAllProperties().get("linea"), "114");
+	}
 
+	
+	@Test
+	public void convertirNodoToPoi() {		
+		Node nodoParada = repoPois.getNodoPoiById(5);
+		POI parada = POIToNodeConverter.convertToPOI(nodoParada);
+		
+		Node nodoCgp = repoPois.getNodoPoiById(7);
+		POI cgp = POIToNodeConverter.convertToPOI(nodoCgp);
+		
+		Node nodoBanco = repoPois.getNodoPoiById(8);
+		POI banco = POIToNodeConverter.convertToPOI(nodoBanco);
+		
+		Node nodoLocal = repoPois.getNodoPoiById(6);
+		POI local = POIToNodeConverter.convertToPOI(nodoLocal);
+		
+		assertEquals(parada.getTipo(), "paradaColectivo");
+		assertEquals(cgp.getTipo(), "cgp");
+		assertEquals(banco.getTipo(), "sucursalBanco");
+		assertEquals(local.getTipo(), "localComercial");
+	}
+	
 }
